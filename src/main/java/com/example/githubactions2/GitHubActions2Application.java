32187eb2131e11a290cbbe2536d30d891564f18d
svanholm200-debug <svanholm200@gmail.com>
2026-02-23 09:23:18 +0100

    first commit

---
+++ src/main/java/com/example/githubactions2/GitHubActions2Application.java
@@ -0,0 +1,13 @@
+package com.example.githubactions2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActions2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActions2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubactions2/GitHubActions2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactions2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActions2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
